219fd57793ca309cb869084746382ea458631c75
mikefab <mikefabrikant@gmail.com>
2016-11-14 11:49:16 -0500

    Add lint

--- app/models/utility.js
+++ app/models/utility.js
@@ -1,34 +1,36 @@
+
+var number_map = {
+  one: 1, two: 2, three: 3, four: 4, five: 5, six: 6, seven: 7, eight: 8, nine: 9
+};
 var config = require('../../config'); // get our config file
 
-function string_to_hash(){
-  string = process.env.id_org || config.id_org
+function string_to_hash() {
+  var string = process.env.id_org || config.id_org;
   var properties = string.split(', ');
   var obj = {};
   properties.forEach(function(property) {
-      var tup = property.split(':');
-      obj[tup[0]] = tup[1];
-  });  
-  return obj
+    var tup = property.split(':');
+    obj[tup[0]] = tup[1];
+  });
+  return obj;
 }
 
-var number_map = {'one' : 1, 'two': 2, 'three': 3, 'four': 4, 'five': 5, 'six': 6, 'seven': 7, 'eight': 8, 'nine': 9}
-
-function clean_phrase(req){
-  phrase = req.params.url.split(/\s+/).map(function(e){
-    return e = !!number_map[e] ? number_map[e] : e
-  })
-  return phrase.join('').toLowerCase()
+function clean_phrase(req) {
+  var phrase = req.params.url.split(/\s+/).map(function(e) {
+    return number_map[e] ? number_map[e] : e;
+  });
+  return phrase.join('').toLowerCase();
 }
 
-function is_image(image_url){
-  return !!image_url.match(/(jpg|png|gif)$/)
+function is_image(image_url) {
+  return image_url.match(/(jpg|png|gif)$/);
 }
 
-function mask(long_string){
-  return long_string.replace(/...................................../, '*************')
+function mask(long_string) {
+  return long_string.replace(/...................................../, '*************');
 }
 
 exports.string_to_hash = string_to_hash;
-exports.clean_phrase   = clean_phrase;
-exports.is_image       = is_image;
-exports.mask           = mask;
+exports.clean_phrase = clean_phrase;
+exports.is_image = is_image;
+exports.mask = mask;
